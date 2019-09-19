da9a9f9e79a3b391718e60480e1682b5b2be264b
GrinningJack <ekcuoc.idroj@gmail.com>
2019-09-19 08:41:49 +0200

    Add files via upload

---
+++ SE/SE_Labo1ConsoleAppGithub/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SE_Labo1ConsoleAppGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Jordi
+    {
+        private int KnapheidsLevel;
+
+        public int _KnapheidsLevel
+        {
+            get { return KnapheidsLevel; }
+            set { KnapheidsLevel = value; }
+        }
+
+    }
+}
